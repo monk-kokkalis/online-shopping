6048791dc671163a4d362c93f764b51cde9af8e4
Christian Will Yngson <christianwillyngson@gmail.com>
2024-02-03 18:41:04 +0800

    added Navbar mobile layout

--- src/app/ui/home/Navbar.tsx
+++ src/app/ui/home/Navbar.tsx
@@ -15,19 +15,19 @@ function Navbar() {
     "Pages"
   ];
   return (
-    <nav className="px-8 py-6 flex items-center justify-between">
-      <div className='flex items-center justify-between gap-16'>
+    <nav className="relative px-8 py-6 flex items-center justify-between">
+      <div className='flex flex-col md:flex-row justify-between gap-16 w-full md:w-auto'>
         <div className="text-[#252B42] font-bold">Bandage</div>
-        <ul className='text-sm text-[#737373] flex gap-5 font-bold'>
+        <ul className='text-sm text-[#737373] flex flex-col md:flex-row gap-5 font-bold items-center'>
           {links.map((link, index) => <Link key={`home-nav-link-${index}`} href="/">{link}</Link>)}
         </ul>
       </div>
-      <div className='flex items-center gap-8'>
-        <div className='text-blue-link flex gap-2 items-center'>
+      <div className='flex absolute md:static items-center gap-8 top-7 right-8'>
+        <div className='hidden text-blue-link md:flex gap-2 items-center'>
           <UserIcon width={12} height={12} />
           <span className='font-bold text-sm'>Login / Register</span>
         </div>
-        <div className='flex items-center gap-6 text-blue-link'>
+        <div className='flex items-center gap-6 text-[#0e1d56] md:text-blue-link'>
           {/* @TODO enable redux connection */}
           <MagnifyingGlassIcon />
           <ShoppingCartIcon />
