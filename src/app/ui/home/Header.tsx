8a541b9b77eeef36151af5acb207a1b6d001391d
Christian Will Yngson <christianwillyngson@gmail.com>
2024-02-03 16:36:04 +0800

    added icons

---
+++ src/app/ui/home/Header.tsx
@@ -0,0 +1,21 @@
+import { EmailIcon, PhoneIcon } from "../icons";
+
+function Header() {
+  return (
+    <div className="bg-green h-[58px] px-8 flex items-center justify-between text-sm text-white">
+      <div className='flex items-center justify-between gap-8'>
+        <div className="flex items-center gap-2">
+          <PhoneIcon />
+          <div className="text-sm text-white">(225) 555-0118</div>
+        </div>
+        <div className="flex items-center gap-2">
+          <EmailIcon />
+          <div className="text-sm text-white">michelle.rivera@example.com</div>
+        </div>
+      </div>
+      <div>Follow Us  and get a chance to win 80% off</div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/app/ui/icons/index.ts
@@ -0,0 +1,14 @@
+export type IconProps = {
+  width?: number;
+  height?: number;
+  className?: string;
+}
+
+export const iconDefaults = {
+  width: 17,
+  height: 17,
+  className: ''
+}
+
+export { EmailIcon } from './EmailIcon';
+export { PhoneIcon } from './PhoneIcon';
